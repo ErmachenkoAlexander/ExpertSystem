1ce6ab2223f06fd3d99a497002179f1dfde1a9c3
ErmachenkoAlexander <a.ermachenko@tools4brokers.com>
2018-04-03 23:51:09 +0300

    Add database connection

--- ES1/Helpers/DBHelper.cs
+++ ES1/Helpers/DBHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ES1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,35 @@ namespace ES1.Helpers
     {
         static DBHelper()
         {
+            try
+            {
+                using (var db = new CitiesDB("con1"))
+                {
+                    foreach (var dt in db.CitiesData)
+                    {
+                        var record = new Dictionary<int, string>()
+                    {
+                        { 1 , "" + dt.Nhemisphere },
+                        { 2 , "" + dt.Whemisphere },
+                        { 3 , dt.Continent },
+                        { 4 , dt.PosContWEC },
+                        { 5 , dt.PosContSNC },
+                        { 6 , dt.Country },
+                        { 7 , "" + dt.PosBySea },
+                        { 8 , dt.PosCountryWEC },
+                        { 9 , dt.PosCountrySNC },
+                        { 10 , dt.Population },
+                        { 11 , dt.Name }
+                    };
 
+                        ParametersValues.Add(record);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
         }
 
         public static List<Dictionary<int, string>> ExcludeByParameter(int parameter, string value, List<Dictionary<int, string>> parametersValues)
@@ -22,12 +51,6 @@ namespace ES1.Helpers
             return parametersValues.Select(x => x[parameter]).Distinct().ToList();
         }
 
-        static public List<Dictionary<int, string>> ParametersValues { get; set; } = new List<Dictionary<int, string>>()
-        {
-            new Dictionary<int, string>() { { 1, "abc"}, { 2, "cde" }, { 3, "cde" } },
-            new Dictionary<int, string>() { { 1, "abd"}, { 2, "cet" }, { 3, "c" } },
-            new Dictionary<int, string>() { { 1, "abc"}, { 2, "cre" }, { 3, "ce" } },
-            new Dictionary<int, string>() { { 1, "abd"}, { 2, "cet" }, { 3, "cd\\e" } },
-        };
+        static public List<Dictionary<int, string>> ParametersValues { get; set; } = new List<Dictionary<int, string>>();
     }
 }
